31dd359d43e1dbcf06ac11d68ef98d6fe14750cb
Paul Mihei <Paul Mihei>
2016-04-21 23:30:02 +0200

    done to uncomplete removeWithoutCopy

--- app/arrays.js
+++ app/arrays.js
@@ -3,19 +3,23 @@ exports = (typeof window === 'undefined') ? global : window;
 exports.arraysAnswers = {
 
   indexOf : function(arr, item) {
-
+    return arr.findIndex((element, index, array) => element === item);
   },
 
   sum : function(arr) {
-
+    return arr.reduce((a,b) => a + b);
   },
 
   remove : function(arr, item) {
-
+    return arr.filter(e => e != item);
   },
 
   removeWithoutCopy : function(arr, item) {
-
+    for(let i = 0; i < arr.length; i++) {
+      if(arr[i] === item)
+        arr.splice(findIndex(arr, item), 1);
+    }
+    return arr.filter(e => e != item);
   },
 
   append : function(arr, item) {
