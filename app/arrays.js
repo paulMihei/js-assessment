b3cf068633e7d0dc3295b6d1faa5b03af42cf77c
Paul Mihei <Paul Mihei>
2016-04-22 18:01:34 +0200

    do to completed square

--- app/arrays.js
+++ app/arrays.js
@@ -23,39 +23,51 @@ exports.arraysAnswers = {
   },
 
   append: function (arr, item) {
-
+    arr.push(item);
+    return arr;
   },
 
   truncate: function (arr) {
-
+    arr.pop();
+    return arr;
   },
 
   prepend: function (arr, item) {
-
+    arr.unshift(item);
+    return arr;
   },
 
   curtail: function (arr) {
-
+    arr.shift();
+    return arr;
   },
 
   concat: function (arr1, arr2) {
-
+    return arr1.concat(arr2);
   },
 
   insert: function (arr, item, index) {
-
+    arr.splice(index, 0, item);
+    return arr;
   },
 
   count: function (arr, item) {
-
+    var count = 0;
+    arr.forEach(e => e === item ? count++ : '');
+    return count;
   },
 
   duplicates: function (arr) {
-
+    var duplicatesArr = [];
+    arr.forEach(e => {
+      if (arraysAnswers.count(arr, e) > 1 && duplicatesArr.indexOf(e) == -1)
+        duplicatesArr.push(e);
+    });
+    return duplicatesArr;
   },
 
   square: function (arr) {
-
+    return arr.map(e => e*e);
   },
 
   findAllOccurrences: function (arr, target) {
