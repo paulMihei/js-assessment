10b2c4f7e96e12b00c34830b33f71f661d60a1fb
Paul Mihei <Paul Mihei>
2016-04-27 17:52:42 +0200

    do to 2nd async ex

--- app/async.js
+++ app/async.js
@@ -1,11 +1,31 @@
 exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
-  async : function(value) {
+  async: function (value) {
 
   },
 
-  manipulateRemoteData : function(url) {
+  manipulateRemoteData: function (url) {
+    return new Promise(function (resolve, reject) {
+      var xobj = new XMLHttpRequest();
+      xobj.overrideMimeType("application/json");
+      xobj.open('GET', url, true);
+      xobj.onreadystatechange = function () {
 
+        if (xobj.readyState == 4 && xobj.status == "200") {
+          var obj = JSON.parse(xobj.responseText);
+          var peopleArr = [];
+
+          for (let i = obj.people.length - 1; i >= 0; i--) {
+            if (obj.people[i].name != 'Matt')
+              peopleArr.push(obj.people[i].name);
+          }
+          peopleArr.splice(2, 0, obj.people[0].name);
+          resolve(peopleArr);
+        }
+
+      };
+      xobj.send(null);
+    });
   }
 };
